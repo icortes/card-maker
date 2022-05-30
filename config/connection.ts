d7d3841f187b93e1fc4fa1d1ec554f50c8d1f956
isa <5445946+icortes@users.noreply.github.com>
2022-05-30 02:54:46 -0700

    updated connection helper

--- config/connection.ts
+++ config/connection.ts
@@ -1,10 +1,20 @@
 import mongoose from 'mongoose';
 
-export default function connect() {
+const connectionState = {
+  isConnected: false,
+};
+
+export default function dbConnect() {
   try {
+    // checks if we are already connected to the db
+    if (connectionState.isConnected) return;
+
     const connection = mongoose.connect(
       process.env.MONGO_URI || 'mongodb://localhost/card-maker-data'
     );
+
+    connectionState.isConnected = true;
+
     console.log('Connected to Cluster.');
 
     return connection;
