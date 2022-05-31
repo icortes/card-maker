176d1dcce7208c18dd2b2169ab627ef098d8adcd
isa <5445946+icortes@users.noreply.github.com>
2022-05-31 02:01:15 -0700

    context provider for supabase in _app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,10 +2,12 @@ import '../styles/globals.css';
 import Head from 'next/head';
 import { AppProps } from 'next/app';
 import { CacheProvider, EmotionCache } from '@emotion/react';
-import CssBaseline  from '@mui/material/CssBaseline';
+import CssBaseline from '@mui/material/CssBaseline';
 import { ThemeProvider } from '@mui/material/styles';
 import theme from '../src/theme';
 import createEmotionCache from '../src/createEmotionCache';
+import { Auth } from '@supabase/ui';
+import { supabase } from '../lib/initSupabase';
 
 // Client-side cache, shared for the whole session of the user in the browser.
 const clientSideEmotionCache = createEmotionCache();
@@ -24,7 +26,9 @@ export default function MyApp(props: MyAppProps) {
       <ThemeProvider theme={theme}>
         {/* CssBaseline kickstart an elegant, consistent, and simple baseline to build upon. */}
         <CssBaseline />
-        <Component {...pageProps} />
+        <Auth.UserContextProvider supabaseClient={supabase}>
+          <Component {...pageProps} />
+        </Auth.UserContextProvider>
       </ThemeProvider>
     </CacheProvider>
   );
