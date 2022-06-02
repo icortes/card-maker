dbb7d2fbc6461b19182daa825e0d57109b304108
isa <5445946+icortes@users.noreply.github.com>
2022-06-02 16:28:44 -0700

    completed sig in form

--- pages/signin.tsx
+++ pages/signin.tsx
@@ -1,5 +1,15 @@
 import { LockOutlined } from '@mui/icons-material';
-import { Avatar, Box, Container, CssBaseline, Typography } from '@mui/material';
+import {
+  Avatar,
+  Box,
+  Button,
+  Container,
+  CssBaseline,
+  Grid,
+  Link,
+  TextField,
+  Typography,
+} from '@mui/material';
 import { FormEvent } from 'react';
 
 export default function SigninPage() {
@@ -28,8 +38,46 @@ export default function SigninPage() {
           onSubmit={handleSubmit}
           noValidate
           sx={{ mt: 1 }}>
-          
-          </Box>
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            id='email'
+            label='Email Address'
+            name='email'
+            autoComplete='email'
+            autoFocus
+          />
+          <TextField
+            margin='normal'
+            required
+            fullWidth
+            name='password'
+            label='Password'
+            type={'password'}
+            id='password'
+            autoComplete='current-password'
+          />
+          <Button
+            type='submit'
+            fullWidth
+            variant='contained'
+            sx={{ mt: 3, mb: 2 }}>
+            Sign in
+          </Button>
+          <Grid container>
+            <Grid item xs>
+              <Link href='#' variant='body2'>
+                Forgot password?
+              </Link>
+            </Grid>
+            <Grid item>
+              <Link href='/signup' variant='body2'>
+                {"Don't have an account? Sign Up"}
+              </Link>
+            </Grid>
+          </Grid>
+        </Box>
       </Box>
     </Container>
   );
